aaef046cd11992360200ccd9ccd4601ce5f529d2
carolinaoliveirasilveira <carolinaoliveirasilveira@outlook.com>
2025-01-29 11:57:16 -0300

    Adds user registration functionality with API

---
+++ src/main/java/com/carolina/frontend/script.js
@@ -0,0 +1,40 @@
+const formulario = document.querySelector("form");
+const Inome = document.querySelector(".nome");
+const Iemail = document.querySelector(".email");
+const Isenha = document.querySelector(".senha");
+const Itel = document.querySelector(".tel");
+
+function cadastrar() {
+    fetch("http://localhost:8080/usuarios", {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            nome: Inome.value,
+            email: Iemail.value,
+            senha: Isenha.value,
+            telefone: Itel.value
+        })
+    })
+    .then(function (res) {
+        console.log(res);
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
+}
+
+function limpar() {
+    Inome.value = "";
+    Iemail.value = "";
+    Isenha.value = "";
+    Itel.value = "";
+}
+
+formulario.addEventListener('submit', function (event) {
+    event.preventDefault();
+    cadastrar();
+    limpar();
+});
